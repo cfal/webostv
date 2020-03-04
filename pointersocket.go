8c0db253c10d36a13fca49c7e8ae85068aab3289
cfal <none>
2020-03-04 19:34:23 +0900

    Add functions for button presses

--- pointersocket.go
+++ pointersocket.go
@@ -1,8 +1,10 @@
 package webostv
 
 import (
+	"errors"
 	"fmt"
 	"github.com/gorilla/websocket"
+	"strconv"
 	"sync"
 )
 
@@ -71,8 +73,6 @@ func (ps *PointerSocket) Input(btype, bname string) (err error) {
 	return ps.writeMessage(websocket.TextMessage, []byte(msg))
 }
 
-// UP DOWN LEFT RIGHT HOME BACK DASH  and numbers
-
 func (ps *PointerSocket) Move(dx, dy int) (err error) {
 	msg := fmt.Sprintf("type:move\ndx:%d\ndy:%d\ndown:0\n\n", dx, dy)
 	return ps.writeMessage(websocket.TextMessage, []byte(msg))
@@ -86,4 +86,100 @@ func (ps *PointerSocket) Scroll(dx, dy int) (err error) {
 func (ps *PointerSocket) Click() (err error) {
 	msg := "type: click\n\n"
 	return ps.writeMessage(websocket.TextMessage, []byte(msg))
+}
+
+// Buttons reference:
+// - https://www.openhab.org/addons/bindings/lgwebos/
+// - https://github.com/TheRealLink/pylgtv/pull/19/files
+// - 
+
+func (ps *PointerSocket) Button(bname string) (err error) {
+	return ps.Input("button", bname)
+}
+
+func (ps *PointerSocket) ButtonUp() (err error) {
+	return ps.Button("UP")
+}
+
+func (ps *PointerSocket) ButtonDown() (err error) {
+	return ps.Button("DOWN")
+}
+
+func (ps *PointerSocket) ButtonLeft() (err error) {
+	return ps.Button("LEFT")
+}
+
+func (ps *PointerSocket) ButtonRight() (err error) {
+	return ps.Button("RIGHT")
+}
+
+func (ps *PointerSocket) ButtonHome() (err error) {
+	return ps.Button("HOME")
+}
+
+func (ps *PointerSocket) ButtonBack() (err error) {
+	return ps.Button("BACK")
+}
+
+func (ps *PointerSocket) ButtonEnter() (err error) {
+	return ps.Button("ENTER")
+}
+
+func (ps *PointerSocket) ButtonDash() (err error) {
+	return ps.Button("DASH")
+}
+
+func (ps *PointerSocket) ButtonInfo() (err error) {
+	return ps.Button("INFO")
+}
+
+func (ps *PointerSocket) ButtonAsterisk() (err error) {
+	return ps.Button("ASTERISK")
+}
+
+func (ps *PointerSocket) ButtonCC() (err error) {
+	return ps.Button("CC")
+}
+
+func (ps *PointerSocket) ButtonExit() (err error) {
+	return ps.Button("EXIT")
+}
+
+func (ps *PointerSocket) ButtonMute() (err error) {
+	return ps.Button("MUTE")
+}
+
+func (ps *PointerSocket) ButtonRed() (err error) {
+	return ps.Button("RED")
+}
+
+func (ps *PointerSocket) ButtonGreen() (err error) {
+	return ps.Button("GREEN")
+}
+
+func (ps *PointerSocket) ButtonBlue() (err error) {
+	return ps.Button("BLUE")
+}
+
+func (ps *PointerSocket) ButtonVolumeUp() (err error) {
+	return ps.Button("VOLUMEUP")
+}
+
+func (ps *PointerSocket) ButtonVolumeDown() (err error) {
+	return ps.Button("VOLUMEDOWN")
+}
+
+func (ps *PointerSocket) ButtonChannelUp() (err error) {
+	return ps.Button("CHANNELUP")
+}
+
+func (ps *PointerSocket) ButtonChannelDown() (err error) {
+	return ps.Button("CHANNELDOWN")
+}
+
+func (ps *PointerSocket) ButtonNumber(num int) (err error) {
+	if num < 0 || num > 9 {
+		return errors.New("Invalid number")
+	}
+	return ps.Button(strconv.Itoa(num))
 }
